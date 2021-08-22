862de957d9ce6b0df700698d66670d04250b73d4
Douglas Naphas <douglasnaphas@gmail.com>
2021-08-21 21:41:45 -0400

    Set itest/test path patterns

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  testPathIgnorePatterns: ["itest"],
+  testRegex: ".*[.]test[.]jsx?",
 };
--- jest.itest.config.js
+++ jest.itest.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  testRegex: ".*[.]itest[.][j]sx?",
+  testRegex: ".*[.]itest[.]jsx?",
 };
